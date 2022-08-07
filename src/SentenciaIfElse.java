e6bb3c32d10f5c04b364b0756959a8ce9b2ecc53
Samantha González <samujm76@gmail.com>
2022-08-07 14:09:35 -0500

    sentencia if else if

--- src/SentenciaIfElse.java
+++ src/SentenciaIfElse.java
@@ -1,12 +1,31 @@
 
 public class SentenciaIfElse {
+
     public static void main(String args[]) {
         var condicion = false;
-        
+
         if (condicion) {
             System.out.println("Condición verdadera");
         } else {
             System.out.println("Condición falsa");
         }
+
+        
+        var numero = 5;
+        var numeroTexto = "Numero desconocido";
+
+        if (numero == 1) {
+            numeroTexto = "Numero uno";
+        } else if (numero == 2) {
+            numeroTexto = "Numero dos";
+        } else if ( numero == 3) {
+            numeroTexto = "Numero tres";
+        } else if ( numero == 4 ) {
+            numeroTexto = "Numero cuatro";
+        } else {
+            numeroTexto = "Numero no encontrado";
+        }
+        
+        System.out.println("numeroTexto = " + numeroTexto);
     }
 }
