74f6735bba092691d7271be50f2987d35776969d
Samantha González <samujm76@gmail.com>
2022-08-06 23:18:03 -0500

    operadores condicionales AND y OR

--- src/OperadoresCondicionales.java
+++ src/OperadoresCondicionales.java
@@ -1,12 +1,27 @@
 
 public class OperadoresCondicionales {
+
     public static void main(String args[]) {
-        var a = 10;
+        var a = -1;
         var valorMinimo = 0;
         var valorMaximo = 10;
-        
+
         var resultado = a >= valorMinimo && a <= valorMaximo;
+
+        if (resultado) {
+            System.out.println("Dentro del rango");
+        } else {
+            System.out.println("Fuera de rango");
+        }
+
+        //Operador or
+        var vacaciones = false;
+        var diaDescanso = true;
         
-        System.out.println("resultado = " + resultado);
+        if (vacaciones || diaDescanso) {
+            System.out.println("Padre puede asistir al juego de su hijo");
+        } else {
+            System.out.println("El padre esta ocupado");
+        }
     }
 }
