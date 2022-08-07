10ddb2babdf20608c850ea915b1c1ed32af1a49a
Samantha González <samujm76@gmail.com>
2022-08-07 17:24:07 -0500

    Ciclo while

---
+++ src/CicloWhile.java
@@ -0,0 +1,12 @@
+
+public class CicloWhile {
+
+    public static void main(String args[]) {
+        var contador = 0;
+        
+        while(contador < 3){
+            System.out.println("contador = " + contador);
+            contador++;
+        }
+    }
+}
