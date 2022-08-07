84e2e0f887781d2e4942e981d977b3e5cb7e6c9b
Samantha González <samujm76@gmail.com>
2022-08-07 18:00:12 -0500

    Atributos y metodos de la clase Persona

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -1,6 +1,19 @@
-
 package clases;
 
 public class Persona { //Para acceder a una clase desde otro archivo esta debe ser public, nombre de la clase debe iniciar en mayuscula
-    
+    //Atributos al inicio
+    //Atributo es una caracteristica que vamos a asociar a nuestra clase
+
+    String nombre;
+    String apellido;
+
+    //Despues los metodos
+    //Metodo es una parte de codigo que vamos a poder reutilizar
+    //se puede mandar llamar tantas veces como se necesitan
+    //puede recibir valores (argumentos)
+    //puede regresar valores (valor de retorno)
+    public void despegarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
 }
