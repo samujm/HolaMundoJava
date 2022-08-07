32f9b23eeff9756ec353bd351bf3a0165be6f28e
Samantha González <samujm76@gmail.com>
2022-08-07 14:03:52 -0500

    sentencia if else

---
+++ src/SentenciaIfElse.java
@@ -0,0 +1,12 @@
+
+public class SentenciaIfElse {
+    public static void main(String args[]) {
+        var condicion = false;
+        
+        if (condicion) {
+            System.out.println("Condición verdadera");
+        } else {
+            System.out.println("Condición falsa");
+        }
+    }
+}
