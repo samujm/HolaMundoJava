01600c334c69cbbf2da70913c6a8d9eb2d7138fe
Samantha González <samujm76@gmail.com>
2022-08-07 17:29:17 -0500

    Ciclo do while

---
+++ src/CicloDoWhile.java
@@ -0,0 +1,19 @@
+
+public class CicloDoWhile {
+
+    public static void main(String args[]) {
+//        var contador = 0;
+//        while(contador < 3){
+//            System.out.println("contador = " + contador);
+//            contador++;
+//        }
+
+        var contador = 0;
+        do{
+            System.out.println("contador = " + contador);
+            contador++; //Se incrementa el contador o se ciclará
+        }while(contador < 0);
+           
+    }
+
+}
