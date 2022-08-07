aeeb784e85e482b78717cf9bc9416694e2bd74e5
Samantha González <samujm76@gmail.com>
2022-08-07 16:38:47 -0500

    Ciclo Switch

---
+++ src/CicloSwitch.java
@@ -0,0 +1,26 @@
+
+public class CicloSwitch {
+
+    public static void main(String args[]) {
+        //Recomendada para menus
+        var numero = 2;
+        var numeroTexto = "Valor desconocido";
+
+        switch (numero) {
+            case 1:
+                numeroTexto = "Numero uno";
+                break;
+            case 2:
+                numeroTexto = "Numero dos";
+                break;
+            case 3:
+                numeroTexto = "Numero tres";
+                break;
+            case 4:
+                numeroTexto = "Numero cuatro";
+                break;
+            default:
+                numeroTexto = "Caso no encontrado";
+        }
+    }
+}
