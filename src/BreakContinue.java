163c16b63d0accae639da5cc61d3883121e9f369
Samantha González <samujm76@gmail.com>
2022-08-07 17:45:06 -0500

    etiqueta goto

--- src/BreakContinue.java
+++ src/BreakContinue.java
@@ -3,10 +3,11 @@ public class BreakContinue {
 
     public static void main(String args[]) {
         
+        inicio: //viene a la linea de codigo que dice continue
         for (var contador = 0; contador < 3; contador++) {
             //Contador ya no puede ser utilizada fuera del ciclo for
             if (contador % 2 != 0) {
-                continue; //Omite las siguientes lineas y continua a la siguiente iteración del ciclo for (en este caso ya no imprime el contador)
+                continue inicio; //Omite las siguientes lineas y continua a la siguiente iteración del ciclo for (en este caso ya no imprime el contador)
             }
             System.out.println("contador = " + contador);
         }
