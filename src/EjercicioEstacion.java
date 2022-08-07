aec989e7999f6d8cc44d34a928af10ab5519c9e1
Samantha González <samujm76@gmail.com>
2022-08-07 14:16:28 -0500

    ejercicio de estacion del año

---
+++ src/EjercicioEstacion.java
@@ -0,0 +1,19 @@
+
+public class EjercicioEstacion {
+
+    public static void main(String args[]) {
+        var mes = 4;
+        var estacion = "Estación desconocida";
+        
+        if (mes == 1 || mes == 2 || mes == 12) {
+           estacion = "Invierno";
+        } else if ( mes == 3 || mes == 4 || mes == 5){
+            estacion = "Primavera";
+        } else if ( mes == 6 || mes == 7 || mes == 8){
+            estacion = "Verano";
+        } else if ( mes == 9 || mes == 10 || mes == 11){
+            estacion = "Otoño";
+        }
+        System.out.println ("Estacion = " + estacion);
+    }
+}
