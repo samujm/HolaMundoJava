6c771eae80d942ca9e217ef826a34c5200f4b467
Samantha González <samujm76@gmail.com>
2022-08-07 16:45:07 -0500

    Ejercicio estaciones del año con Switch

---
+++ src/EjercicioEstacionSwitch.java
@@ -0,0 +1,26 @@
+
+public class EjercicioEstacionSwitch {
+    public static void main(String args[]) {
+        var mes = 1;
+        var estacion = "Estacion desconocida";
+        
+        switch(mes){
+            case 1: case 2: case 12:
+                estacion = "Invierno";
+                break;
+             case 3: case 4: case 5:
+                estacion = "Primavera";
+                break;
+            case 6: case 7: case 8:
+                estacion = "Verano";
+                break;
+            case 9: case 10: case 11:
+                estacion = "Otoño";
+                break;
+            default:
+                estacion = "Estacion desconocida";
+        }
+        System.out.println("Estacion = " + estacion);
+    }
+}
+
