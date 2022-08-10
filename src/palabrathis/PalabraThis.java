da7b534585244284826d77b83600b710f5c44d0e
Samantha González <samujm76@gmail.com>
2022-08-09 22:25:50 -0500

    THIS VA APUNTANDO A LOS OBJETOS SEGUN EL OBJETO QUE SE ESTÁ EJECUTANDO EN ESE MOMENTO

--- src/palabrathis/PalabraThis.java
+++ src/palabrathis/PalabraThis.java
@@ -1,5 +1,6 @@
 package palabrathis;
 
+//THIS VA APUNTANDO A LOS OBJETOS SEGUN EL OBJETO QUE SE ESTÁ EJECUTANDO EN ESE MOMENTO
 public class PalabraThis {
 
     public static void main(String[] args) {
@@ -19,7 +20,23 @@ class Persona { //Las otras clases ya no pueden ser publicas, solo puede existir
     String apellido;
 
     Persona(String nombre, String apellido) {
+        //super(); //constructor de la clase padre, de la clase object, es implicita su llamada
         this.nombre = nombre;
         this.apellido = apellido;
+        System.out.println("Objeto persona usando this: " + this); //primero se ejecuta el objeto por eso sale primero
+        new Imprimir().imprimir(this); //Creando un nuevo objeto que ya regresa una referencia, no es necesario asignarla a una variable
     }
+}
+
+
+class Imprimir{
+    
+    public Imprimir(){
+        super(); //El constructor de la clase objetct (padre) para reservar memoria
+    }
+    
+    public void imprimir(Persona persona){
+        System.out.println("persona desde imprimir = " + persona);
+        System.out.println("impresion del objeto actual (this) = " + this); //IMPRIME EL OBJETO QUE ESTA EJECUTANDO EN ESE MOMENTO, OSEA IMPRMIR
+    } 
 }
